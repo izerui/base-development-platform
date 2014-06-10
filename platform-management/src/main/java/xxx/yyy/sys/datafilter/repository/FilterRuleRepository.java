036e5dc49df9836aae46584b020ab07b479606ae
Administrator <liuyuhua69@gmail.com>
2014-06-10 17:03:29 +0800

    @Repository

--- platform-management/src/main/java/xxx/yyy/sys/datafilter/repository/FilterRuleRepository.java
+++ platform-management/src/main/java/xxx/yyy/sys/datafilter/repository/FilterRuleRepository.java
@@ -1,10 +1,12 @@
 package xxx.yyy.sys.datafilter.repository;
 
+import org.springframework.stereotype.Repository;
 import xxx.yyy.sys.base.jpa.PlatformJpaRepository;
 import xxx.yyy.sys.datafilter.model.FilterRule;
 
 /**
  * Created by serv on 14-6-4.
  */
+@Repository
 public interface FilterRuleRepository extends PlatformJpaRepository<FilterRule>{
 }
